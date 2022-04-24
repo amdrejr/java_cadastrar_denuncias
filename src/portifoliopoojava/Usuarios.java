15a323f6ae6e241a81af786b681269670a48fc83
André <andrejr.mello@hotmail.com>
2022-04-24 00:36:02 -0300

    Usuarios classe

--- src/portifoliopoojava/Usuarios.java
+++ src/portifoliopoojava/Usuarios.java
@@ -10,13 +10,14 @@ import java.util.Map;
 /**
  *
  * @author Andre Jr Mello
+ * 
  */
 public abstract class Usuarios {
 
-    // acesso - senha
+    
     static Map<String, String> cadastrados = new HashMap<String, String>()
         {
-            {
+            {   // acesso - senha
                 put("andre", "12345");
                 put("julia", "12345");
                 put("sandro", "12345");
